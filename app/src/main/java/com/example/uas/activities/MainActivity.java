cd1d2954015f442cc27637d152235f1b7c3ce3d5
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-12-25 13:58:29 +0700

    Fix "fragment" & "Splash"

--- app/src/main/java/com/example/uas/activities/MainActivity.java
+++ app/src/main/java/com/example/uas/activities/MainActivity.java
@@ -4,16 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.uas.R;
 import com.example.uas.fragments.*;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,28 +71,52 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         Fragment fragment = null;
         switch (menuItem.getItemId()) {
             case R.id.action_list:
-                fragment = new ListFragment();
-                break;
+                openFragment(new ListFragment());
+                return true;
             case R.id.action_edit:
-                fragment = new UpdateFragment();
-                break;
+                openFragment(new UpdateFragment());
+                return true;
             case R.id.action_add:
-                fragment = new AddFragment();
-                break;
+                openFragment(new AddFragment());
+                return true;
             case R.id.action_delete:
-                fragment = new DeleteFragment();
-                break;
+                openFragment(new DeleteFragment());
+                return true;
             case R.id.action_about:
-                fragment = new AboutFragment();
-                break;
+                openFragment(new AboutFragment());
+                return true;
         }
         return loadFragment(fragment);
     }
 
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
+    private void openFragment(Fragment fragment) {
+        openFragment(fragment, false);
     }
 
+    private void openFragment(Fragment fragment, boolean addToBackstack) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        if (addToBackstack)
+            transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
 }
